942889bf909469b434deefd57bcc1db3b9e82015
EbuCodes <ebucodes@gmail.com>
2022-06-02 21:09:34 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, outputs strings
+ * Description: Using the main function
+ * to print out strings.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
