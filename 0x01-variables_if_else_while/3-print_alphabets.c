4a4bb18e76c824f50d3ac91d15dc86bf3dc928c7
EbuCodes <ebucodes@gmail.com>
2022-06-03 21:24:57 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the letters of
+ * the alphabet in both uppser and lower cases.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
