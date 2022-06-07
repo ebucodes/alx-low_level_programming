6d6c4a411844aa0888c30623777b21f92383b596
EbuCodes <ebucodes@gmail.com>
2022-06-07 12:53:32 +0100

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function is used to add
+ * two integers and return the result.
+ * @a: nteger to input
+ * @b: integer to input
+ *
+ * Return: addition of numbers.
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
+
