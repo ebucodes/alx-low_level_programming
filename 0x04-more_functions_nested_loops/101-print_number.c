1a15959ce9dd05eed8d24b0b4532ded85228d0cb
EbuCodes <ebucodes@gmail.com>
2022-06-09 21:17:10 +0100

    Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputed integer
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+	return;
+}
